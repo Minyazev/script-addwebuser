e08decdf14ea3d297590c9d269067eaa565c0fc4
Minyazev <48429421+Minyazev@users.noreply.github.com>
2019-03-11 09:40:08 +0300

    Create script.bash
    this script only show how to read argument from command line during start script

---
+++ script.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# в данном примере скрипта показано, как прочитать аргумент запуска скрипта
+
+echo "Lets's Rock!"
+
+site_name=$1
+
+echo "site name=$site_name"
+
+echo "Vse putem!"
